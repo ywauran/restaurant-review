60a0ce2dd761b5e038b60e7eccdba289be6dc8f6
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-05-04 10:27:11 +0800

    third commit

--- app/src/main/java/com/ywauran/restaurantreview/ApiConfig.kt
+++ app/src/main/java/com/ywauran/restaurantreview/ApiConfig.kt
@@ -7,11 +7,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object{
-        fun getApiService(): ApiService{
-            val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
-            val retrofit = Retrofit.Builder().baseUrl("https://restaurant-api.dicoding.dev/").addConverterFactory(GsonConverterFactory.create()).client(client).build()
-
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://restaurant-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
             return retrofit.create(ApiService::class.java)
         }
     }
--- app/src/main/java/com/ywauran/restaurantreview/ApiService.kt
+++ app/src/main/java/com/ywauran/restaurantreview/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("detail/{id")
+    @GET("detail/{id}")
     fun getRestaurant(
         @Path("id") id: String
     ): Call<RestaurantResponse>
