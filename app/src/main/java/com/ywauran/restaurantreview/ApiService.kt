3cd339aa924d11c519705f8375f299584d041955
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-05-04 09:14:52 +0800

    first commit

---
+++ app/src/main/java/com/ywauran/restaurantreview/ApiService.kt
@@ -0,0 +1,12 @@
+package com.ywauran.restaurantreview
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("detail/{id")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
